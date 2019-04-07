3de976b3a43ddb0edb8ec9dff38564579b757f2d
Angela Gadon <apgadon@gmail.com>
2019-04-07 02:19:56 -0700

    Further UI modifications. No API calls yet

--- inject.js
+++ inject.js
@@ -13,8 +13,9 @@
 	*/
 	var cssvar_inj = document.createElement('style');
 	cssvar_inj.textContent = ':root { --z: 99998; --z2: 99999;'
-	    + '--color1: #f1c40f; --color2: #f39c12; --color3: #3c3e50; --color4: #44495e;}'
+	    + '--color1: #efd5ff; --color2: #515ada; --color3: #3c3e50; --color4: #44495e;}'
 		+ 'h1 { padding: 10px; font-family: "Segoe UI", Tahoma, sans-serif; font-weight: 200; }'
+		+ '#mainextwindow * {color:#f2f2f2 !important}'	//selects all extension text
 		;
 	
 	document.head.appendChild(cssvar_inj);
@@ -25,21 +26,21 @@
 	document.head.appendChild(encoding_inj);
 
 	var div_inj = document.createElement('div');
-	div_inj.setAttribute("style", "z-index: var(--z); position: fixed; top: 10px; right: 10px;  width: 357px; height: 357px; border: 15px solid var(--color4); border-radius:1%; border-bottom-left-radius: 10%; border-bottom-color: var(--color3); border-left-color: var(--color3); background-color: var(--color4); color: white;");
+	div_inj.setAttribute("style", "z-index: var(--z); position: fixed; top: 10px; right: 10px;  width: 357px; height: 357px; background: linear-gradient(90deg, var(--color3) 0%, var(--color4) 100%); border: 15px solid var(--color4); border-radius:1%; border-bottom-left-radius: 10%; border-bottom-color: var(--color3); border-left-color: var(--color3); background-color: var(--color4);");
 	div_inj.setAttribute("id", "mainextwindow");
 	document.body.appendChild(div_inj);
 	//THESE (div_inj and extbtn_inj) HAVE TO BE NEXT TO EACH OTHER
 	//exit button
 	var extbtn_inj = document.createElement('button');
 	extbtn_inj.textContent = '×';
-	extbtn_inj.setAttribute("style", "z-index: var(--z2); position:absolute; border: 5px solid var(--color4); background-color:var(--color4); border-bottom-left-radius: 50%; border-bottom-color: var(--color3); border-left-color: var(--color3); top:-2%; left:90%; font-weight: bold; color:white;");
+	extbtn_inj.setAttribute("style", "z-index: var(--z2); position:absolute; border: 5px solid var(--color4); background-color:var(--color4); border-bottom-left-radius: 50%; border-bottom-color: var(--color3); border-left-color: var(--color3); top:-2%; left:90%; font-weight: bold;");
 	extbtn_inj.setAttribute("id", "exit");
 	div_inj.appendChild(extbtn_inj);
 
 	//content
 	var title_inj = document.createElement('h1');
 	title_inj.textContent = "Here's a Title";
-	title_inj.setAttribute("style", " padding: 10px; font-family: \"Segoe UI\", Tahoma, sans-serif; font-weight: 200; color: white");
+	title_inj.setAttribute("style", " padding: 10px; font-family: \"Segoe UI\", Tahoma, sans-serif; font-weight: 100;");
 	div_inj.appendChild(title_inj);
 
 
--- popitup.js
+++ popitup.js
@@ -1,9 +1,15 @@
 // this is the background code...
 
 // listen for our browerAction to be clicked
+var clicked_bool = false;
 chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'inject.js'
-	});
+	clicked_bool = !clicked_bool;
+	console.write(clicked_bool);
+	if(clicked_bool==true){
+		chrome.tabs.executeScript(tab.ib, {
+			file: 'inject.js'
+		});
+	}
+	else
+		alert("WHA");
 });
