456e0d3632d97a9968c507d17af436196fc34123
Angela Gadon <apgadon@gmail.com>
2019-04-07 01:41:08 -0700

    Working window with placeholder title and exit button

---
+++ inject.js
@@ -0,0 +1,67 @@
+
+(function() {
+	/*
+	const cssurl = chrome.runtime.getURL('\/stylesheet.css');
+	alert(cssurl);
+	doSomething(cssurl);
+	function doSomething(desfile){
+		alert(desfile);
+		var css_link = document.createElement("SCRIPT");
+		css_link.setAttribute("src", desfile);
+		document.body.appendChild(css_link);
+	}
+	*/
+	var cssvar_inj = document.createElement('style');
+	cssvar_inj.textContent = ':root { --z: 99998; --z2: 99999;'
+	    + '--color1: #f1c40f; --color2: #f39c12; --color3: #3c3e50; --color4: #44495e;}'
+		+ 'h1 { padding: 10px; font-family: "Segoe UI", Tahoma, sans-serif; font-weight: 200; }'
+		;
+	
+	document.head.appendChild(cssvar_inj);
+
+	//<meta charset="UTF-8">
+	var encoding_inj = document.createElement('meta');
+	encoding_inj.setAttribute("charset", "UTF-8");
+	document.head.appendChild(encoding_inj);
+
+	var div_inj = document.createElement('div');
+	div_inj.setAttribute("style", "z-index: var(--z); position: fixed; top: 10px; right: 10px;  width: 357px; height: 357px; border: 15px solid var(--color4); border-radius:1%; border-bottom-left-radius: 10%; border-bottom-color: var(--color3); border-left-color: var(--color3); background-color: var(--color4); color: white;");
+	div_inj.setAttribute("id", "mainextwindow");
+	document.body.appendChild(div_inj);
+	//THESE (div_inj and extbtn_inj) HAVE TO BE NEXT TO EACH OTHER
+	//exit button
+	var extbtn_inj = document.createElement('button');
+	extbtn_inj.textContent = '×';
+	extbtn_inj.setAttribute("style", "z-index: var(--z2); position:absolute; border: 5px solid var(--color4); background-color:var(--color4); border-bottom-left-radius: 50%; border-bottom-color: var(--color3); border-left-color: var(--color3); top:-2%; left:90%; font-weight: bold; color:white;");
+	extbtn_inj.setAttribute("id", "exit");
+	div_inj.appendChild(extbtn_inj);
+
+	//content
+	var title_inj = document.createElement('h1');
+	title_inj.textContent = "Here's a Title";
+	title_inj.setAttribute("style", " padding: 10px; font-family: \"Segoe UI\", Tahoma, sans-serif; font-weight: 200; color: white");
+	div_inj.appendChild(title_inj);
+
+
+	//listener for exit button, deletes window
+	extbtn_inj.addEventListener("click", function(){
+		var div_rm = document.getElementById("mainextwindow");
+		div_rm.innerHTML = "";
+		div_rm.setAttribute("style", "display:none;");
+		var bluh = div_rm.parentNode;
+		bluh.removeChild(div_rm);
+	});
+
+	
+
+
+
+})();
+
+/*TEMPLATE
+
+var _inj = document.createElement('');
+_inj.textContent = ;
+_inj.setAttribute("style", "");
+div_inj.appendChild(_inj);
+*/
